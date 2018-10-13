8e4572053eb4ff449f0c3608d0b889813e590f67
Carlos Tovar B <catovar360@gmail.com>
2018-10-12 19:36:22 -0500

    feat: adding API

--- py_moduler/urls.py
+++ py_moduler/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Statistics API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^', include('reportes.urls')),
     url(r'^', include('tareas.urls')),
     url(r'^', include('estadisticas.urls')),
+    url(r'^$', schema_view),
 ]
