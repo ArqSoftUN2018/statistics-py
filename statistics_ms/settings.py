5dbb8f6a49fe19f03b019e38536522bb34228bff
caatovarbo <catovar360@gmail.com>
2018-11-08 10:08:55 -0500

    Update settings.py

--- statistics_ms/settings.py
+++ statistics_ms/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
         'NAME': 'statistics',
         'USER': 'statistics',
         'PASSWORD': 'statistics',
-        'HOST': 'statistics-db',
-        'PORT': '',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
